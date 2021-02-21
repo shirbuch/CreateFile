9ad4deac20fd8ee0f1ddc1148514daf46d429f44
shirbuch <shirbuchner@yahoo.com>
2021-02-21 18:24:08 +0200

    tchar_strcpy implemented

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "windows.h"
+#include <stdint.h>
 
-#define MAX_INPUT_LEN (32767)
+#define MAX_INPUT_LEN (200) //!!!(32767)
 #define RESTRICTED_PATH "c:\folderstopsecret"
 #define NUM_ARGS (2)
 
 int main(int argc, char* argv[])
 {
-	TCHAR path[MAX_INPUT_LEN];
-	TCHAR fileName[MAX_INPUT_LEN];
+	TCHAR path[MAX_INPUT_LEN + 1];
+	TCHAR fileName[MAX_INPUT_LEN + 1];
 
 	if (argc - 1 != NUM_ARGS)
 	{
@@ -20,4 +21,24 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+int8_t tchar_strcpy(TCHAR *to, TCHAR *from)
+{
+	size_t i;
+	for (i = 0; from[i] != '\0'; ++i)
+	{
+		// Check that we aren't sliping out of the buffer
+		if (i > MAX_INPUT_LEN)
+		{
+			return -1;
+		}
+
+		// Copy a char
+		to[i] = from[i];
+	}
+
+	// Save '\0' at the end of 'to'
+	to[i] = '\0';
+
+	return 0;
+}
 
