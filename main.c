48b0b495fa291c6a3e7cb8c03357d2e1339a30fa
shirbuch <shirbuchner@yahoo.com>
2021-02-21 18:01:18 +0200

    sln files

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "windows.h"
 
 #define MAX_INPUT_LEN (32767)
 #define RESTRICTED_PATH "c:\folderstopsecret"
 
-int main()
+int main(int argc, char* argv[])
 {
-	char* path;
-	char* fileName;
+	TCHAR* path;
+	TCHAR* fileName;
+
+	printf_s("argc: %d\n", argc);
+	for (size_t i = 1; i < argc; ++i)
+	{
+		printf_s("%s\n", argv[i]);
+	}
 
 	return 0;
 }
