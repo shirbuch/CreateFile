ea061b241d0e9d26129c1cab1ce4cefaf1e53a5b
shirbuch <shirbuchner@yahoo.com>
2021-02-21 16:03:22 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_LEN (32767)
+#define RESTRICTED_PATH "c:\folderstopsecret"
+
+int main()
+{
+	char* path;
+	char* fileName;
+
+	return 0;
+}
