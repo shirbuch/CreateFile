ee592d2fde01d734e541fdf96224a4342982c5d3
shirbuch <shirbuchner@yahoo.com>
2021-02-22 11:05:47 +0200

    is_prefix implemented

--- main.c
+++ main.c
@@ -5,12 +5,15 @@
 #include <wchar.h>
 #include <fcntl.h>
 #include <io.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define MAX_INPUT_LEN (200) //!!!(32767)
-#define RESTRICTED_PATH "c:\folderstopsecret"
+#define RESTRICTED_PATH ((TCHAR *) L"c:\folderstopsecret")
 #define NUM_ARGS (2)
 
 int8_t tchar_strcpy(TCHAR* to, TCHAR* from);
+bool is_prefix(TCHAR* str, TCHAR* prefix);
 
 int wmain(int argc, TCHAR* argv[])
 {
@@ -24,7 +27,7 @@ int wmain(int argc, TCHAR* argv[])
 		return -1;
 	}
 
-	// Try coping the path and file name from argv
+	// Copy the path and file name from argv
 	if (tchar_strcpy(path, argv[NUM_ARGS - 1]) || tchar_strcpy(fileName, argv[NUM_ARGS]))
 	{
 		printf_s("Error in reading arguments.\n");
@@ -38,12 +41,29 @@ int wmain(int argc, TCHAR* argv[])
 	wprintf(L"%ls\n%ls\n", path, fileName);
 	*/
 
+	// Check if the path is valid
+	if (is_prefix(path, RESTRICTED_PATH))
+	{
+		printf_s("Unexeptable path.\n");
+		return -1;
+	}
+
+
+
 	return 0;
 }
 
 int8_t tchar_strcpy(TCHAR *to, TCHAR *from)
 {
 	size_t i;
+
+	// Check if the pointers are NULL
+	if (to == NULL || from == NULL)
+	{
+		return -1;
+	}
+
+	// Copy the 'from' string to the 'to' string
 	for (i = 0; from[i] != '\0'; ++i)
 	{
 		// Check that we aren't sliping out of the buffer
@@ -62,3 +82,23 @@ int8_t tchar_strcpy(TCHAR *to, TCHAR *from)
 	return 0;
 }
 
+bool is_prefix(TCHAR* str, TCHAR* prefix)
+{
+	// Check if the pointers are NULL
+	if (str == NULL || prefix == NULL)
+	{
+		return false;
+	}
+
+	// Check if the prefix exist in the string
+	for (size_t i = 0; prefix[i] != '\0'; ++i)
+	{
+		// Check if we finished the string or non-matching chars
+		if (str[i] == '\0' || str[i] != prefix[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
